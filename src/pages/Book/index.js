9f5bf4c199e72a50ac76d3eac424b28f911be3af
Gui-dev <adrianon10@gmail.com>
2020-02-02 12:51:31 -0300

    first commit

---
+++ src/pages/Book/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View } from 'react-native'
+
+function Book() {
+
+  return (
+    <View>
+      <Text>Book</Text>
+    </View>
+  )
+}
+
+export default Book
---
+++ src/pages/Login/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text, TextInput } from 'react-native'
+
+function Login() {
+
+  return (
+    <View>
+      <Text>E-mail</Text>
+      <TextInput />
+    </View>
+  )
+}
+
+export default Login
